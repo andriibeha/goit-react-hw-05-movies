87396c606892187dd0f260418ab02a647e349a4d
andriibeha <beha.andrii@gmail.com>
2022-07-20 16:53:13 +0300

    qwe

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes} from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 import Header from "components/Header/Header";
 import Home from "components/pages/Home/Home";
 import Movie from "components/pages/Movie/Movie";
@@ -6,17 +6,17 @@ import NotFound from "components/pages/NotFound/NotFound";
 import TrendItem from "components/TrendItem/TrendItem";
 
 
-function App() { 
+function App() {
     return (
         <>
-        <Routes >
-            <Route path="/" element={<Header />}>
-                <Route index element={<Home />} /> 
-                <Route path="movie" element={<Movie />} />
-                <Route path="/movie/:id/*" element={<TrendItem />} />
-                <Route path="*" element={<NotFound />} />
-            </Route>
-        </Routes>
+            <Routes >
+                <Route path="/" element={<Header />}>
+                    <Route index element={<Home />} />
+                    <Route path="movie" element={<Movie />} />
+                    <Route path="/movie/:id/*" element={<TrendItem />} />
+                    <Route path="*" element={<NotFound />} />
+                </Route>
+            </Routes>
         </>
     );
 };
